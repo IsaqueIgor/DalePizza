9a0f8987cfb659d63bdc66f39eba87d72c318557
Isaque Igor <isaqueigorsilva@gmail.com>
2022-02-11 22:46:25 -0300

    feat: add keyboardavoidView

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
 import Button from 'src/components/button';
 import Input from 'src/components/input';
-import { GradientBackground } from './styles';
+
+import { Content, GradientBackground } from './styles';
 
 const Signin = () => {
   return (
     <GradientBackground>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
 
-      <Input placeholder="Password" type="secondary" secureTextEntry />
+          <Input placeholder="Password" type="secondary" secureTextEntry />
 
-      <Button title="Sign in" type="secondary" />
+          <Button title="Sign in" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </GradientBackground>
   );
 };
--- src/screens/Signin/styles.ts
+++ src/screens/Signin/styles.ts
@@ -1,6 +1,5 @@
 import styled, { css } from 'styled-components/native'
-import { LinearGradient } from 'react-native-linear-gradient'
-import { View } from 'react-native';
+import {  getBottomSpace } from 'react-native-iphone-x-helper'
 
 export const GradientBackground = styled.View`
   flex: 1;
@@ -10,3 +9,24 @@ export const GradientBackground = styled.View`
 		background-color: ${theme.COLORS.PRIMARY_900};
 	`}
 `;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace() + 48
+  }
+})`
+  width: 100%;
+  padding: 0 32px;
+`
+export const Title = styled.Text`
+  font-size: 32px;
+  margin-bottom:24px;
+  align-self: flex-start;
+
+  ${({ theme }) => css`
+		font-family: ${theme.FONTS.TITLE};
+		color: ${theme.COLORS.TITLE};
+	`}
+
+`
