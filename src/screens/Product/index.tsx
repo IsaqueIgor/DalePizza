db04ae7bfd21cedd369b77121a6d6daea6872014
Isaque Igor <isaqueigorsilva@gmail.com>
2022-02-15 22:03:04 -0300

    feat: add delete label

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { Platform } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import { Container, Header, Title } from './styles';
+import { Container, Header, Title, DeleteLabel } from './styles';
 
 const Product = () => {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
         <Title>Register</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Remove</DeleteLabel>
+        </TouchableOpacity>
       </Header>
     </Container>
   );
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -18,6 +18,14 @@ export const Title = styled.Text`
     color: ${theme.COLORS.TITLE};
   `}
 `
+export const DeleteLabel = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.TEXT};
+  `}
+`
 
 export const Header = styled(LinearGradient).attrs(({theme}) => ({
   colors: theme.COLORS.GRADIENT
