b2a4c1f082225c19471293cda4835716f13fb7f3
Isaque Igor <isaqueigorsilva@gmail.com>
2022-02-25 20:13:48 -0300

    feat: fetch pizzas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { Alert, TouchableOpacity, FlatList } from 'react-native';
@@ -6,11 +6,42 @@ import { useTheme } from 'styled-components/native';
 import { ProductProps } from 'src/components/ProductCard';
 
 import { Container, Header, Greeting, GreetingText } from './styles';
+import { useAuth } from 'src/hooks/auth';
 
 const Home = () => {
   const [pizzas, setPizzas] = useState<ProductProps[]>([]);
   const [search, setSearch] = useState('');
   const { COLORS } = useTheme();
+  const { user, signOut } = useAuth();
+
+  const fetchPizzas = async (value: string): Promise<void> => {
+    const formattedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection('pizzas')
+      .orderBy('name_insensitive')
+      .startAt(formattedValue)
+      .endAt(`${formattedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+        setPizzas(data);
+      })
+      .catch(() =>
+        Alert.alert('Consulta', 'Não foi possível realizar a consulta.')
+      );
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPizzas('');
+    }, [])
+  );
 
   return (
     <Container>
@@ -19,6 +50,11 @@ const Home = () => {
           <GreetingText>Olá, Admin</GreetingText>
         </Greeting>
       </Header>
+
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemsNumber>{pizzas.length} pizzas</MenuItemsNumber>
+      </MenuHeader>
     </Container>
   );
 };
